aa72605dddecb05c01cb5400411467c5f44b11e8
Josh Carr <josh.carr@aclimalabs.com>
2014-12-08 14:44:06 -0800

    adds some tests

--- test/spec.js
+++ test/spec.js
@@ -1,21 +1,31 @@
-/*global test:false, ok:false */
+/*global test:false, ok:false, largestTriangleThreeBuckets */
 
-(function () {
+( function() {
   'use strict';
 
-  test('Library reduces length of data array to within threshold', function () {
+  var sampleObjectData = [],
+    sampleArrayData = [],
+    d = Date.now(),
+    i = 0;
 
-    var data = [];
-    var d = Date.now();
-    for( var i=1; i < 200000; i++ ){
-      data.push({
-        date: d - i,
-        value: i + Math.random()
-      });
-      // data.push([d - i, Math.random() + i ]);
-    }
+  for ( i = 1; i < 2000; i++ ) {
+    sampleObjectData.push({
+      date: d - i,
+      value: i + Math.random()
+    });
+    sampleArrayData.push( [ d - i, Math.random() + i ] );
+  }
 
-    ok( largestTriangleThreeBuckets( data, 1440, 'date', 'value' ).length <= 1440, 'Returned data is <= threshold' );
+  test( 'Library reduces length of data array to within threshold', function() {
+    ok( largestTriangleThreeBuckets( sampleObjectData, 1440, 'date', 'value' ).length <= 1440, 'Returned data is <= threshold' );
   });
 
-}());
+  test( 'Library can use array-based Data', function() {
+    ok( largestTriangleThreeBuckets( sampleArrayData, 1440, 0, 1 ).length <= 1440, 'Returned data is <= threshold' );
+  });
+
+  test( 'Library does not downsample if the passed threshold is zero', function() {
+    ok( largestTriangleThreeBuckets( sampleArrayData, 0, 0, 1 ).length === sampleArrayData.length, 'Returned data is the same length as original data' );
+  });
+
+}() );
